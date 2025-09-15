a9138f99fc894cb08a2c7b882d1feabd0db82b94
Venkata Ramana Govindu <venkataramanagovindu2015@gmail.com>
2025-09-14 21:35:54 -0500

    Refactor CORS settings to remove localhost fallback for FRONTEND_URL in API handlers

--- backend/api/reviews/hostaway.js
+++ backend/api/reviews/hostaway.js
@@ -5,7 +5,7 @@ import { loadApprovals } from '../../lib/approvals.js';
 
 export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "http://localhost:5173"); 
+    res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL); 
     res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
   
