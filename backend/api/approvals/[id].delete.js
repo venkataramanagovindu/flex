e11f09370626323450720f3eb0ecb80eeafea0fa
Venkata Ramana Govindu <venkataramanagovindu2015@gmail.com>
2025-09-14 21:24:56 -0500

    Update CORS settings to use a fixed FRONTEND_URL in approvals and reviews API handlers

--- backend/api/approvals/[id].delete.js
+++ backend/api/approvals/[id].delete.js
@@ -2,7 +2,7 @@ import { unapproveReview } from '../../lib/approvals.js';
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL); 
+  res.setHeader("Access-Control-Allow-Origin", "https://flex-frontend-five.vercel.app"); 
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
   res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
 
